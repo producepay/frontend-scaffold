50097cb810ea436598e22f0fe228bf5897f43475
mchin88 <melanie@producepay.com>
2019-06-21 13:26:17 -0700

    use extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,20 @@
-let defaultConfig = require('tailwindcss/defaultConfig');
-
 module.exports = {
   theme: {
-    colors: {
-      primary: '#5CB65B',
-      ...defaultConfig.theme.colors,
-    },
-    spacing: {
-      ...defaultConfig.theme.spacing,
-      '100': '25rem',
-    },
-    fontSize: {
-      ...defaultConfig.theme.fontSize,
-      'xxs-xs': '.6875rem', // 11px
-      'xs-sm': '.8125rem', // 13px
-      '6xl': '4rem',
-    },
-    lineHeight: {
-      ...defaultConfig.theme.lineHeight,
-      base: '1.15',
+    extend: {
+      colors: {
+        primary: '#5CB65B',
+      },
+      spacing: {
+        '100': '25rem',
+      },
+      fontSize: {
+        'xxs-xs': '.6875rem', // 11px
+        'xs-sm': '.8125rem', // 13px
+        '6xl': '4rem',
+      },
+      lineHeight: {
+        base: '1.15',
+      },
     },
   },
-  variants: {
-    ...defaultConfig.variants,
-  },
-  corePlugins: {},
-  plugins: [],
 };
