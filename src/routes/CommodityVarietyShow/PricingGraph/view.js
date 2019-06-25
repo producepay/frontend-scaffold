16436ff3ce4f40633f2de32b9d764f6dfef4c9e9
Jake Dluhy <jake.dluhy@producepay.com>
2019-06-25 11:11:57 -0700

    Add ability to track shipping point changes

--- src/routes/CommodityVarietyShow/PricingGraph/view.js
+++ src/routes/CommodityVarietyShow/PricingGraph/view.js
@@ -19,7 +19,7 @@ function filterPriceReportsBySku(priceReports, sku) {
 }
 
 function PricingGraphView(props) {
-  const { priceReports } = props;
+  const { commodityName, priceReports } = props;
 
   const uniqReports = uniqBy(priceReports, 'skuName');
   const skuOptions = uniqReports.map(r => ({ label: skuNameWithoutVariety(r), value: r.skuName }));
@@ -43,6 +43,11 @@ function PricingGraphView(props) {
         filterPriceReportsBySku(priceReports, sku),
       ),
     );
+  }
+
+  function onShippingPointChange(shippingPoints) {
+    trackEvent(KEYS.PRICING_GRAPH_SHIPPING_POINT_CHANGE, { commodityName, shippingPoints })
+    setActiveShippingPoints(shippingPoints);
   }
 
   return (
@@ -74,7 +79,7 @@ function PricingGraphView(props) {
             <PriceLineGraph
               priceReportsForSku={activePriceReports}
               activeItems={activeShippingPoints}
-              onChange={values => setActiveShippingPoints(values)}
+              onChange={onShippingPointChange}
             />
           </div>
         </React.Fragment>
@@ -84,6 +89,7 @@ function PricingGraphView(props) {
 }
 
 PricingGraphView.propTypes = {
+  commodityName: PropTypes.string.isRequired,
   priceReports: PropTypes.arrayOf(
     PropTypes.shape({
       cityName: PropTypes.string,
