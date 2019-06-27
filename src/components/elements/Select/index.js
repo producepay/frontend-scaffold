4dd72c052f4a307d006830e4f0fe329626e557b4
Jake Dluhy <jake.dluhy@producepay.com>
2019-06-27 10:20:52 -0700

    Switch from defaultValue to value

--- src/components/elements/Select/index.js
+++ src/components/elements/Select/index.js
@@ -21,7 +21,7 @@ function Select(props) {
       <select
         className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none"
         onChange={onChange}
-        defaultValue={_.get(selectedItem, 'value')}
+        value={_.get(selectedItem, 'value')}
       >
         {items.map((item) => (
           <option
