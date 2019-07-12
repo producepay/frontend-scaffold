3a062bb03dfb0642f292f886e61a6c0358cf45c6
Jake Dluhy <jake.dluhy@producepay.com>
2019-07-12 12:40:05 -0700

    Remove map and show pricing breakdown on mobile by default

--- src/routes/CommodityVarietyShow/PricingDetails/index.js
+++ src/routes/CommodityVarietyShow/PricingDetails/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import gql from 'graphql-tag';
-import { useQuery } from 'react-apollo-hooks';
 import _ from 'lodash';
 
 import { nameFromUuids } from '../../../helpers/commodities-and-varieties';
-import { gqlF, getUTCDate } from '../../../helpers/dates';
 import { getLatestReportsAndDate } from '../../../helpers/reports';
 
 import PricingDetailsView from './view';
-
-const FETCH_DAILY_REPORT_CONTENT = gql`
-  query LatestDailyReportContent(
-    $commodityId: String!,
-    $varietyId: String,
-    $startDate: Date,
-    $endDate: Date
-  ) {
-    latestDailyReportContent(
-      commodityId: $commodityId,
-      varietyId: $varietyId,
-      startDate: $startDate,
-      endDate: $endDate
-    ) {
-      commodityId
-      varietyId
-      pricingMapUrl
-    }
-  }
-`;
 
 function PricingDetails(props) {
   const { commodityId, varietyId, pricingData } = props;
@@ -35,23 +12,11 @@ function PricingDetails(props) {
   const commodityVarietyName = nameFromUuids(commodityId, varietyId);
   const [latestReportDate, latestReports] = getLatestReportsAndDate(_.filter(pricingData, 'resolvedAveragePrice'));
 
-  const { data } = useQuery(FETCH_DAILY_REPORT_CONTENT, {
-    variables: {
-      commodityId,
-      varietyId,
-      startDate: gqlF(getUTCDate(latestReportDate)),
-      endDate: gqlF(getUTCDate(latestReportDate)),
-    },
-    context: { clientName: 'insightsPlatform' },
-  });
-
   return (
     <PricingDetailsView
       commodityVarietyName={commodityVarietyName}
-      allReports={pricingData}
       latestReports={latestReports || []}
       latestReportDate={latestReportDate}
-      pricingMapUrl={_.get(data, 'latestDailyReportContent.pricingMapUrl', null)}
     />
   );
 }
