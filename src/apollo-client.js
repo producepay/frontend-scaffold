aacc626734535eb1794abbadf2e6a52e62ef3e10
mchin88 <melanie@producepay.com>
2019-09-13 11:35:36 -0700

    pass in token through header

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,5 +1,6 @@
 import { ApolloLink } from 'apollo-link';
 import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
 import _ from 'lodash';
 
 ////// NOTE: Since we have 2 different APIs to connect to:
@@ -19,11 +20,23 @@ const insightsPlatformLink = new HttpLink({
   headers: {},
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 export default new ApolloClient({
   link: ApolloLink.split(
     operation => operation.getContext().clientName === "insightsPlatform", // Routes the query to the proper client
     insightsPlatformLink,
-    dataPlatformLink
+    authLink.concat(dataPlatformLink)
   ),
   cache: new InMemoryCache(),
 });
