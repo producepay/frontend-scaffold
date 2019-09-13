e93497894a677c3e1b85d9f421074409cc4dfb7b
mchin88 <melanie@producepay.com>
2019-09-12 17:41:17 -0700

    temporary sign out

--- src/routes/Insights/index.js
+++ src/routes/Insights/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useAuth } from '../../contexts/auth';
 
 export default () => {
+  const { setAuthToken } = useAuth();
+
   return (
     <div className='p-8'>
       Welcome to insights!
+      <div className='pt-4'>
+        <button onClick={() => {
+          localStorage.removeItem('authToken');
+          setAuthToken(null);
+        }}>
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 }
