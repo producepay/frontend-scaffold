375bb26c43c4d36960a5a17bc4d4c8bba618093b
mchin88 <melanie@producepay.com>
2019-10-03 16:31:11 -0700

    update th element props

--- src/components/elements/table/TH/index.js
+++ src/components/elements/table/TH/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 function TH(props) {
-  const { className, size, children, ...rest } = props;
+  const { className, size, bgColor, color, children, ...rest } = props;
 
-  const cName = cx(className, `py-3 px-3 bg-primary text-white text-${size} font-semibold tracking-wide`);
+  const cName = cx(className, `py-3 px-3 bg-${bgColor} text-${color} text-${size} font-semibold tracking-wide`);
 
   return (
     <th
@@ -20,10 +20,14 @@ function TH(props) {
 
 TH.propTypes = {
   size: PropTypes.oneOf(['xs', 'xs-sm', 'sm', 'sm-base', 'base']),
+  bgColor: PropTypes.string, // any tailwind .bg color suffix
+  color: PropTypes.string,   // any tailwind .text color suffix
 };
 
 TH.defaultProps = {
   size: 'sm',
+  bgColor: 'primary',
+  color: 'white',
 };
 
 export default React.memo(TH);
