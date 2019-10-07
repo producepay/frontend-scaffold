719770403e65225cc1dc50b35d65dbcd850ec05a
Kevin Taylor <ktp925@gmail.com>
2019-10-07 15:57:03 -0700

    Add market insights all view and query/mutation

--- src/contexts/auth/index.js
+++ src/contexts/auth/index.js
@@ -17,6 +17,7 @@ const LOGIN = gql`
         id
         email
         accessToken
+        unsubscribedAt
       }
     }
   }
--- src/routes/market-insights/MarketInsights/MarketInsightsDashboard/view.js
+++ src/routes/market-insights/MarketInsights/MarketInsightsDashboard/view.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-function MarketInsightsDashboardView() {
+function MarketInsightsDashboardView(props) {
+  const {
+    loading,
+    error,
+    data,
+  } = props;
+
+  if (loading) return 'Loading...';
+  if (error) return `Error: ${error.message}`;
+
+  const listCommodities = data.userCommodityVarietyPreferences.map((preference) => {
+    if(preference.commodityVarietyInfo.variety.id != null) {
+      return <li key={preference.id}>
+        {preference.commodityVarietyInfo.commodity.id}: {preference.commodityVarietyInfo.name}
+      </li>
+    } else {
+      return <li key={preference.id}>
+        {preference.commodityVarietyInfo.commodity.id}: {preference.commodityVarietyInfo.name}
+      </li>
+    }
+  });
+
   return (
-    <div>
+    <div className='p-4'>
       MarketInsightsDashboardView
+      {listCommodities}
     </div>
   );
 }
