59f60764a0fbf0045fd1d8be6805f31417a90c12
Kevin Taylor <ktp925@gmail.com>
2019-10-07 15:57:03 -0700

    Add market insights all view and query/mutation

--- src/routes/market-insights/MarketInsights/view.js
+++ src/routes/market-insights/MarketInsights/view.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { useAuth } from '../../../contexts/auth';
 
 import routes from '../../../routes';
 
@@ -7,15 +8,31 @@ import MarketInsightsDashboard from './MarketInsightsDashboard';
 import MarketInsightsAll from './MarketInsightsAll';
 
 function MarketInsightsView() {
+
+  function handleSubscription() {
+    setSubscribed(!subscribed)
+  }
+
+  let { user } = useAuth()
+  const [subscribed, setSubscribed] = useState(user.unsubscribedAt)
+
   return (
     <React.Fragment>
+      <div className='flex'>
+        <div className='p-4'>Market Insights</div>
+        {subscribed ? (
+          <div className='p-4 cursor-pointer' onClick={handleSubscription}>Unsubscribe to Email</div>
+        ) : (
+          <div className='p-4 cursor-pointer' onClick={handleSubscription}>Subscribe to Email</div>
+        )}
+      </div>
       <div>
-        <Link to={routes.marketInsightsDashboard()}>Your Watch List</Link>
-        <Link to={routes.marketInsightsAll()}>All</Link>
+        <Link className='p-4' to={routes.marketInsightsDashboard()}>Your Watch List</Link>
+        <Link className='p-4' to={routes.marketInsightsAll()}>All</Link>
       </div>
 
       <Switch>
-        <Route exact path={routes.marketInsightsDashboard()} component={MarketInsightsDashboard} />
+        <Route exact path={routes.marketInsightsDashboard()} render={() => <MarketInsightsDashboard foo={"bar"}/>} />
         <Route exact path={routes.marketInsightsAll()} component={MarketInsightsAll} />
 
         <Redirect to={routes.marketInsightsDashboard()} />
