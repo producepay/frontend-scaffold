3129f1595b1cc9e80d9fe58abafb786a18f2fff5
mchin88 <melanie@producepay.com>
2019-10-09 10:45:21 -0700

    table props

--- src/routes/commodities/ShowCommodity/ShowCommodityCustomers/table.js
+++ src/routes/commodities/ShowCommodity/ShowCommodityCustomers/table.js
@@ -3,26 +3,26 @@ import Table from '../../../../components/elements/table';
 import TH from '../../../../components/elements/table/TH';
 import { formatPrice, formatLoads } from '../../../../helpers/format';
 
-function CustomersTable({ groupedSalesOrderLineItems }) {
-  const thProps = { align: 'left', weight: 'medium', size: 'xxs-xs' };
+const TH_PROPS = { align: 'left', weight: 'medium', size: 'xxs-xs' };
 
+function CustomersTable({ groupedSalesOrderLineItems }) {
   return (
     <div>
       <Table color="secondary" className="w-full">
         <thead>
           <tr>
-            <TH {...thProps}>Name</TH>
-            <TH {...thProps}>Quantity Sold</TH>
-            <TH {...thProps}>Sales Amount</TH>
-            <TH {...thProps}>Total Profit</TH>
-            <TH {...thProps}>Avg Cost Per Box</TH>
-            <TH {...thProps}>Avg Sales Price Per Box</TH>
+            <TH {...TH_PROPS}>Name</TH>
+            <TH {...TH_PROPS}>Quantity Sold</TH>
+            <TH {...TH_PROPS}>Sales Amount</TH>
+            <TH {...TH_PROPS}>Total Profit</TH>
+            <TH {...TH_PROPS}>Avg Cost Per Box</TH>
+            <TH {...TH_PROPS}>Avg Sales Price Per Box</TH>
           </tr>
         </thead>
         <tbody>
           {
-            groupedSalesOrderLineItems.map((item, index) => (
-              <tr key={`row-${index}`}>
+            groupedSalesOrderLineItems.map((item) => (
+              <tr key={`${item.groupedValue}`}>
                 <td>{item.groupedValue}</td>
                 <td>{formatLoads(item.shipmentQuantity)}</td>
                 <td>{formatPrice(item.totalSaleAmount, false)}</td>
