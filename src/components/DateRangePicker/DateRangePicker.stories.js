1e30f4b2208331d96e1f16bf80b0b58ca8107302
mchin88 <melanie@producepay.com>
2019-10-08 17:40:17 -0700

    wip date range picker

--- src/components/DateRangePicker/DateRangePicker.stories.js
+++ src/components/DateRangePicker/DateRangePicker.stories.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { actions } from '@storybook/addon-actions';
 
 import DateRangePicker from './index';
 
 storiesOf('Components/DateRangePicker', module)
-.add('basic', () => < DateRangePicker numberOfMonths={2} onDayClick={() => {actions('onDayChange'); console.log('hello');}} />);
+.add('basic', () => < DateRangePicker numberOfMonths={2} />);
--- src/components/DateRangePicker/index.js
+++ src/components/DateRangePicker/index.js
@@ -1,38 +1,78 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import DayPicker, { DateUtils } from 'react-day-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import { DateUtils } from 'react-day-picker';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import TextField from '../elements/TextField';
+
 import 'react-day-picker/lib/style.css';
 
+// const DateInputButton = (props) => {
+//   const { from, to, ref, onClick, ...rest } = props;
+//   const [showCalendar, setShowCalendar] = useState(false);
+//   useEffect(() => {
+//     console.log('showCalender', showCalendar);
+//   }, [showCalendar]);
+
+
+//   return (
+//     <Button
+//       label={from && to ? `${format(from, 'YYYY/MM/DD')} - ${format(to, 'YYYY/MM/DD')}` : "Select a date range"}
+//       onClick={() => {console.log('onClick'); setShowCalendar(!showCalendar);}}
+//       {...rest}
+//     />
+//   )
+// }
+
+function formatDate(date, formatStr) {
+  return format(date, formatStr);
+}
+
 const DateRangePicker = ({
+  className,
   defaultFrom,
   defaultTo,
-  onDayClick,
   ...rest
 }) => {
   const [from, setFrom] = useState(defaultFrom);
   const [to, setTo] = useState(defaultTo);
-
   const handleDayClick = useCallback((day) => {
     const range = DateUtils.addDayToRange(day, { from, to });
     setFrom(range.from);
     setTo(range.to);
   }, [from, to]);
 
+  // const formatDate = useCallback((date, formatStr) => {
+  //   return `${format(from, formatStr)}-${format(to, formatStr)}`;
+  // }, [from, to]);
+
   return (
-    <DayPicker
-      selectedDays={{from, to}}
-      onDayClick={handleDayClick}
-      {...rest}
-    />
+    <div className={className}>
+      <DayPickerInput
+        formatDate={formatDate}
+        format="YYYY/MM/DD"
+        parseDate={parse}
+        component={(props) => <TextField {...props} />}
+        hideOnDayClick={false}
+        dayPickerProps={{
+          selectedDays: {from, to},
+          onDayClick: handleDayClick,
+          ...rest,
+        }}
+      />
+    </div>
   )
 }
 
 DateRangePicker.propTypes = {
+  className: PropTypes.string,
   defaultFrom: PropTypes.instanceOf(Date),
   defaultTo: PropTypes.instanceOf(Date),
 }
 
 DateRangePicker.defaultProps = {
+  className: '',
   defaultFrom: undefined,
   defaultTo: undefined,
 }
