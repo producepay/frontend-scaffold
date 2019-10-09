a70ee7dd896592be49a45d489fe172c75ed65d09
mchin88 <melanie@producepay.com>
2019-10-09 14:31:19 -0700

    some basic config for datepicker

--- src/components/DateRangePicker/index.js
+++ src/components/DateRangePicker/index.js
@@ -2,12 +2,32 @@ import 'react-dates/initialize';
 import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { DateRangePicker } from 'react-dates';
+import moment from 'moment';
+import subWeeks from 'date-fns/sub_weeks';
+import subDays from 'date-fns/sub_days';
+import Button from '../elements/Button';
 
 import 'react-dates/lib/css/_datepicker.css';
 import './datepicker.css';
 
 const START_DATE_ID = "startDate";
 const END_DATE_ID = "endDate";
+
+const DatePresets = ({ presets, onDatesChange }) => {
+  return (
+    <div>
+      {presets.map(({ label, start, end }) => (
+        <Button
+          key={label}
+          variant="outlined"
+          className="ml-6 mb-2"
+          label={label}
+          onClick={() => { onDatesChange({ startDate: start, endDate: end }) }}
+        />
+      ))}
+    </div>
+  );
+}
 
 const Picker = ({
   className,
@@ -26,23 +46,49 @@ const Picker = ({
     setFocusInput(focusedInput); // sets whether start or end input is in focus
   }, []);
 
+  const presets = [
+    {
+      label: "Today",
+      start: moment(),
+      end: moment(),
+    },
+    {
+      label: "Yesterday",
+      start: moment().subtract(1, 'days'),
+      end: moment().subtract(1, 'days'),
+    },
+    {
+      label: "Last Week",
+      start: moment().subtract(1, 'weeks'),
+      end: moment(),
+    },
+    {
+      label: "Last 30 Days",
+      start: moment().subtract(30, 'days'),
+      end: moment(),
+    },
+  ];
+
   return (
     <DateRangePicker
       {...rest}
-      startDateId={START_DATE_ID}
-      endDateId={END_DATE_ID}
-      focusedInput={focusedInput}
-      onDatesChange={onDatesChange}
-      onFocusChange={onFocusChange}
-      startDate={startDate}
-      endDate={endDate}
+      renderCalendarInfo={() => <DatePresets presets={presets} onDatesChange={onDatesChange} />}
+      isOutsideRange={day => moment().diff(day) < 0}
+      initialVisibleMonth={() => moment().subtract(1, 'months')}
+      startDateId={START_DATE_ID} // required
+      endDateId={END_DATE_ID} // required
+      focusedInput={focusedInput} // required
+      onDatesChange={onDatesChange} // required
+      onFocusChange={onFocusChange} // required
+      startDate={startDate} // required
+      endDate={endDate} // required
     />
   )
 }
 
 Picker.propTypes = {
   className: PropTypes.string,
-  initialStartDate: PropTypes.instanceOf(Date),
+  initialStartDate: PropTypes.instanceOf(Date), // todo: change this to instance of moment
   initialEndDate: PropTypes.instanceOf(Date),
 }
 
