cdef79877805c485e73f00051eae879be296acdd
mchin88 <melanie@producepay.com>
2019-10-10 11:56:55 -0700

    fix closing of datepicker

--- src/components/DateRangePicker2/index.js
+++ src/components/DateRangePicker2/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback} from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import { DateUtils } from 'react-day-picker';
@@ -66,21 +66,22 @@ const CalendarMenu = ({
   );
 }
 
-const InputButton = ({from, to, ...props}) => {
+const InputButton = React.forwardRef((props, ref) => {
+  const { from, to, ...rest } = props;
   return (
     <Button
+      ref={ref}
       label={from && to ? formatDateRange(from, to, FORMAT_DATE_STRING) : 'Please select a date range'}
-      {...props}
+      {...rest}
     />
   )
-}
+});
 
 function isSelectingFirstDay(from, to, day) {
   const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
   const isRangeSelected = from && to;
   return !from || isBeforeFirstDay || isRangeSelected;
 }
-
 
 const DateRangePicker = ({
   className,
@@ -91,6 +92,9 @@ const DateRangePicker = ({
   const [from, setFrom] = useState(defaultFrom);
   const [to, setTo] = useState(defaultTo);
   const [enteredTo, setEnteredTo] = useState(defaultTo); // Keep track of the last day for mouseEnter
+  const [showPicker, setShowPicker] = useState(false);
+  const datePickerNode = useRef(null);
+  const dayPickerInput = useRef(null);
 
   const handleDayClick = useCallback((day) => {
     if (from && to && day >= from && day <= to) {
@@ -113,33 +117,53 @@ const DateRangePicker = ({
     if (!isSelectingFirstDay( from, to, day )) {
       setEnteredTo(day);
     }
-  }, [from, to])
+  }, [from, to]);
+
+  const handleClick = useCallback((e) => {
+    if (datePickerNode.current && datePickerNode.current.contains(e.target)) {
+      setShowPicker(true);
+      return;
+    }
+
+    setShowPicker(false);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick, false);
+  }, [handleClick]);
 
   return (
-    <DayPickerInput
-      overlayComponent={({ children, ...props }) => (
-        <CalendarMenu
-          setFrom={setFrom}
-          setTo={setTo}
-          setEnteredTo={setEnteredTo}
-          {...props}
-        >
-          {children}
-        </CalendarMenu>
-      )}
-      component={(props) => <InputButton from={from} to={to} {...props} />}
-      hideOnDayClick={false}
-      dayPickerProps={{
-        className: "InsightsDatePicker",
-        month: subMonths(new Date(), 1),
-        disabledDays: { after: new Date() },
-        selectedDays: [from, { from, to: enteredTo }],
-        modifiers: { start: from, end: enteredTo },
-        onDayClick: handleDayClick,
-        onDayMouseEnter: handleMouseEnter,
-        ...rest,
-      }}
-    />
+    <div>
+      <div ref={datePickerNode} className="inline-block">
+        <DayPickerInput
+          ref={dayPickerInput}
+          overlayComponent={({ children, ...props }) => (
+            <CalendarMenu
+              setFrom={setFrom}
+              setTo={setTo}
+              setEnteredTo={setEnteredTo}
+              {...props}
+            >
+              {children}
+            </CalendarMenu>
+          )}
+          inputProps={{ from, to }}
+          component={InputButton}
+          hideOnDayClick={false}
+          showOverlay={showPicker}
+          dayPickerProps={{
+            className: "InsightsDatePicker",
+            month: subMonths(new Date(), 1),
+            disabledDays: { after: new Date() },
+            selectedDays: [from, { from, to: enteredTo }],
+            modifiers: { start: from, end: enteredTo },
+            onDayClick: handleDayClick,
+            onDayMouseEnter: handleMouseEnter,
+            ...rest,
+          }}
+        />
+      </div>
+    </div>
   )
 }
 
