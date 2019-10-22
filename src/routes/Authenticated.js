1e249db760d5aebc6b3698ddfcbdc8f9c6deb256
mchin88 <melanie@producepay.com>
2019-10-22 12:23:57 -0700

    move filter provider

--- src/routes/Authenticated.js
+++ src/routes/Authenticated.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import routes from '../routes';
-import { FilterStateProvider } from '../contexts/FilterState';
 
 import Dashboard from './Dashboard';
 import MarketInsights from './market-insights/MarketInsights';
@@ -11,16 +10,14 @@ import ShowCommodity from './commodities/ShowCommodity';
 
 function Authenticated(props) {
   return (
-    <FilterStateProvider>
-      <Switch>
-        <Route exact path={routes.dashboard()} component={Dashboard} />
-        <Route path={routes.marketInsights()} component={MarketInsights} />
-        <Route path={routes.showCustomer()} component={ShowCustomer} />
-        <Route path={routes.showCommodity()} component={ShowCommodity} />
+    <Switch>
+      <Route exact path={routes.dashboard()} component={Dashboard} />
+      <Route path={routes.marketInsights()} component={MarketInsights} />
+      <Route path={routes.showCustomer()} component={ShowCustomer} />
+      <Route path={routes.showCommodity()} component={ShowCommodity} />
 
-        <Redirect to={routes.dashboard()} />
-      </Switch>
-    </FilterStateProvider>
+      <Redirect to={routes.dashboard()} />
+    </Switch>
   );
 }
 
